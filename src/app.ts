c09ea7ccb30e194559bfb34985dde0a310a65c49
Shivansh Agarwal <shivansh.aeyron@gmail.com>
2023-11-23 03:17:03 +0530

    disable cross origin request by default

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,15 @@ const authRouter = require('./routes/authRoutes');
 const app: Application = express();
 
 app.use(express.json());
-const cors = require('cors');
+// disable cross origin request by default
+// const cors = require('cors');
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 
-const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 app.use('/api/auth', authRouter);
+
 export default app;
